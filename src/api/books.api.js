d232e9c9ead70812aa2f9cdb258027c553866cc6
bjade <bjade@gracenote.com>
2019-03-21 00:50:54 +0530

    api changes moved from cors to nodejs server

--- src/api/books.api.js
+++ src/api/books.api.js
@@ -1,73 +1,60 @@
-import {API_URL} from '../config';
+import { API_URL } from '../config';
 import axios from 'axios';
 import XMLToJson from '../helper/XMLTOJSON';
 
-const qs = require('qs');
-const API_KEY = process.env.REACT_APP_API_KEY;
-const SEARCH_BOOKS = `${API_URL}search/index.xml?`
-const BOOK_DETAILS = `${API_URL}book/show/?`
+const SEARCH_BOOKS = `${API_URL}search/books/?`;
+const BOOK_DETAILS = `${API_URL}bookDetails?`;
 
 //https://www.goodreads.com/search/index.xml?key=Zdzfd8gLfAybmDaaC2F1mA&q=fifty
 export function searchBooks(params = {}) {
-    params['key'] = API_KEY;
-    // params['q'] = 'fifty';
-    // console.log(qs.stringify(params));
-    return axios.get(SEARCH_BOOKS + qs.stringify(params), {
-            headers: {"X-Requested-With": "XMLHttpRequest", "Access-Control-Allow-Origin": '*'},
-            // crossDomain: true
-        }
-    ).then(function (response) {
+    params['path'] = 'search/index.xml';
+    return axios.get(SEARCH_BOOKS, {
+            'params': params,
+        },
+    ).then(function(response) {
 
         const parser = new DOMParser();
-        const XMLResponse = parser.parseFromString(response.data, "application/xml");
+        const XMLResponse = parser.parseFromString(response.data,
+            'application/xml');
 
-        const parseError = XMLResponse.getElementsByTagName("parsererror");
+        const parseError = XMLResponse.getElementsByTagName('parsererror');
 
         if (parseError.length) {
 
-            // this.setState({
-            //     error: "There was an error fetching results.",
-            //     fetchingData: false
-            // });
-            return Promise.reject("XML Parsing Error");
+            return Promise.reject('XML Parsing Error');
             //throw new Error("XML Parsing Error");
         } else {
             return XMLToJson(XMLResponse).GoodreadsResponse;
         }
 
-    })
-        .catch(function (error) {
-            return Promise.reject(error);
-        })
+    }).catch(function(error) {
+        return Promise.reject(error);
+    });
 }
 
 export function getBookDetails(params = {}) {
-    params['key'] = API_KEY;
+
+    params['path'] = 'book/show/';
     params['format'] = 'xml';
-    return axios.get(BOOK_DETAILS + qs.stringify(params), {
-            headers: {"X-Requested-With": "XMLHttpRequest", "Access-Control-Allow-Origin": '*'},
-        }
-    ).then(function (response) {
+    return axios.get(BOOK_DETAILS, {
+            'params': params,
+        },
+    ).then(function(response) {
 
         const parser = new DOMParser();
-        const XMLResponse = parser.parseFromString(response.data, "text/xml");
+        const XMLResponse = parser.parseFromString(response.data, 'text/xml');
 
-        const parseError = XMLResponse.getElementsByTagName("parsererror");
+        const parseError = XMLResponse.getElementsByTagName('parsererror');
 
         if (parseError.length) {
 
-            // this.setState({
-            //     error: "There was an error fetching results.",
-            //     fetchingData: false
-            // });
-            return Promise.reject("XML Parsing Error");
+            return Promise.reject('XML Parsing Error');
             //throw new Error("XML Parsing Error");
         } else {
             return XMLToJson(XMLResponse).GoodreadsResponse;
         }
 
-    })
-        .catch(function (error) {
-            return Promise.reject(error);
-        })
+    }).catch(function(error) {
+        return Promise.reject(error);
+    });
 }
--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 //https://www.goodreads.com/search/index.xml?key=Zdzfd8gLfAybmDaaC2F1mA&q=fifty
 const config = {
     'cors_url': window.location.protocol + '//cors-anywhere.herokuapp.com',
-    // 'cors_url': 'https://crossorigin.me',
-    'base_url': window.location.protocol + '//www.goodreads.com',
+//explicity set dev development protocol to http
+    'protocol_scheme': process.env.NODE_ENV === 'development'
+        ? 'http:'
+        : window.location.protocol,
+    'base_url': process.env.REACT_APP_BASE_URL,
 
-}
-export const API_URL = `${config.cors_url}/${config.base_url}/`;
+};
+export const API_URL = `${config.protocol_scheme}//${config.base_url}/`;
