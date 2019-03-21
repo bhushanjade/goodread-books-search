9e71831f01b2d2b3b38ebe88af641459bb8dd12a
bjade <bjade@gracenote.com>
2019-03-21 11:57:32 +0530

    protocol changes

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
 //https://www.goodreads.com/search/index.xml?key=Zdzfd8gLfAybmDaaC2F1mA&q=fifty
-console.log("env",process.env.NODE_ENV);
+
 const config = {
     'cors_url': window.location.protocol + '//cors-anywhere.herokuapp.com',
 //explicity set dev development protocol to http
-    'protocol_scheme': process.env.NODE_ENV === 'development'
-        ? 'http:'
-        : window.location.protocol,
+    'protocol_scheme':window.location.protocol,
     'base_url': process.env.REACT_APP_BASE_URL,
 
 };
