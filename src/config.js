5064f3701323fc33e91a83fda492eb0bed5def82
bjade <bjade@gracenote.com>
2019-03-21 11:45:40 +0530

    debug env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 //https://www.goodreads.com/search/index.xml?key=Zdzfd8gLfAybmDaaC2F1mA&q=fifty
+console.log("env",process.env.NODE_ENV);
 const config = {
     'cors_url': window.location.protocol + '//cors-anywhere.herokuapp.com',
 //explicity set dev development protocol to http
